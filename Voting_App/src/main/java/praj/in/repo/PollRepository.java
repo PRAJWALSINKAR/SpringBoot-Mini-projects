6cdc97ae5cea6f467b23ecb4bc38caa0b01ba7b5
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-04-22 09:24:56 +0530

    Delete Voting_App directory

--- Voting_App/src/main/java/praj/in/repo/PollRepository.java
+++
@@ -1,9 +0,0 @@
-package praj.in.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import praj.in.entity.Poll;
-
-public interface PollRepository extends JpaRepository<Poll, Long > {
-
-}
